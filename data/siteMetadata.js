f75d49d6ca6987c7aa3951cc6a9a15ad906a9ee1
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2025-02-27 08:22:17 +0530

    Logo Add and fotter changed

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Code With Leo Blog',
+  author: 'Lode With Leo',
+  headerTitle: 'CodeWithLeo',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
